3398e4dc73b5799e2423e4a10d3d5cf691cf0b19
joshua <joshuasend1@gmail.com>
2022-02-09 17:08:14 +0000

    Introduce matchFirst() on Seekable iterators, and use it only in Procedures for now

--- common/iterator/sorted/SortedIterator.java
+++ common/iterator/sorted/SortedIterator.java
@@ -22,6 +22,8 @@ import com.vaticle.typedb.core.common.iterator.FunctionalIterator;
 
 import java.util.Iterator;
 import java.util.NavigableSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -127,6 +129,16 @@ public interface SortedIterator<T extends Comparable<? super T>, ORDER extends S
         Seekable<T, ORDER> filter(Predicate<T> predicate);
 
         Seekable<T, ORDER> limit(long limit);
+
+        default Seekable<T, ORDER> noNulls() {
+            return filter(Objects::nonNull);
+        }
+
+        default Optional<T> matchFirst(T value) {
+            seek(value);
+            if (peek().equals(value)) return Optional.of(next());
+            else return Optional.empty();
+        }
 
         <U extends Comparable<? super U>, ORD extends Order> Seekable<U, ORD> mapSorted(Function<T, U> mappingFn, Function<U, T> reverseMappingFn, ORD order);
 
