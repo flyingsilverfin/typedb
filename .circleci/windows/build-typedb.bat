625d7da2d4c63d122ec7d272e4c27f0a89a95999
joshua <joshua@vaticle.com>
2023-02-09 13:04:18 +0000

    Update windows bazel build command

--- .circleci/windows/build-typedb.bat
+++ .circleci/windows/build-typedb.bat
@@ -1,7 +1,7 @@
 CALL refreshenv
 
 REM build typedb-all-windows archive
-bazel --output_user_root=C:/_bzl build --enable-runfiles //rust:typedb-binary-mac
+bazel --output_user_root=C:/_bzl build --enable_runfiles //rust:typedb-binary-mac
 
 :error
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
