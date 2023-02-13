f99fdb17ae6dbe18a6cb698910eea8ca5dc44d9a
joshua <joshua@vaticle.com>
2023-02-13 12:22:01 +0000

    Use unicode paths

--- .circleci/windows/build-typedb.bat
+++ .circleci/windows/build-typedb.bat
@@ -1,7 +1,7 @@
 CALL refreshenv
 
 REM build typedb-all-windows archive
-bazel --output_base=C:\_b --output_user_root=C:\_c build --enable_runfiles //rust:typedb-binary-mac --verbose_failures --experimental_ui_max_stdouterr_bytes=999999999
+bazel --output_base=\\?\C:\_b --output_user_root=\\?\C:\_c build --enable_runfiles //rust:typedb-binary-mac --verbose_failures --experimental_ui_max_stdouterr_bytes=999999999
 
 :error
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
