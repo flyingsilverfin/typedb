a37e1db564721215e6e3f4d38f7cf300b02d5894
joshua <joshua@vaticle.com>
2023-02-22 10:32:37 +0000

    Make .cargo/config.toml

--- .circleci/windows/build-typedb.bat
+++ .circleci/windows/build-typedb.bat
@@ -1,10 +1,20 @@
 CALL refreshenv
 
 REM prepare the Cargo files
+mkdir .cargo
+cd .cargo
+(
+    echo "[net]"
+    echo "git-fetch-with-cli = true"
+) > config.toml
+cd ..
+
 bazel run @vaticle_dependencies//tool/cargo:sync
 
 REM build typedb-all-windows archive
 cd rust
+
+
 cargo build
 
 
