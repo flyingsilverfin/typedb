cf066d8cb81ceccceab407c33904de290b065e38
joshua <joshua@vaticle.com>
2023-03-13 10:32:21 +0000

    Try using bat script from circleci command

--- .circleci/windows/build-typedb.bat
+++ .circleci/windows/build-typedb.bat
@@ -17,17 +17,7 @@ REM
 
 CALL refreshenv
 
-REM Use platform's git instead of built-in.
-REM Equivalent to setting `net.git-fetch-with-cli = true` for crate_repositories defined in @vaticle_dependencies
-set CARGO_NET_GIT_FETCH_WITH_CLI=true
-
-REM Prepare cargo manifests
-bazel run @vaticle_dependencies//tool/cargo:sync
-
-cd rust
-cargo build --release
-
-mv target/release/typedb-server-binary.exe .
+bazel run //rust:typedb-server-binary-windows --action_env=path="%PATH%" --action_env=ProgramData=%ProgramData% --action_env=BUILD_WORKSPACE_DIRECTORY=%cd% --action_env=CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 :error
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
