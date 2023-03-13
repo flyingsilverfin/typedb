9db3831e4314a42cfb9355e4bb3c659e7dde2a17
joshua <joshua@vaticle.com>
2023-03-13 12:41:57 +0000

    Without CALL

--- .circleci/windows/build-typedb.bat
+++ .circleci/windows/build-typedb.bat
@@ -19,8 +19,8 @@ CALL refreshenv
 
 CALL setx CARGO_NET_GIT_FETCH_WITH_CLI true
 set CARGO_NET_GIT_FETCH_WITH_CLI=true
-CALL bazel run @vaticle_dependencies//tool/cargo:sync
-CALL bazel run //rust:typedb-server-binary-windows --action_env=path="%PATH%" --action_env=ProgramData=%ProgramData% --action_env=BUILD_WORKSPACE_DIRECTORY=%cd% --action_env=CARGO_NET_GIT_FETCH_WITH_CLI=true
+bazel run @vaticle_dependencies//tool/cargo:sync
+bazel run //rust:typedb-server-binary-windows --action_env=path="%PATH%" --action_env=ProgramData=%ProgramData% --action_env=BUILD_WORKSPACE_DIRECTORY=%cd% --action_env=CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 :error
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
